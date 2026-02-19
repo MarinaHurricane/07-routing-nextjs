f86cf01ce9f112f4d3cedf4cd1218d8f8f263323
MarinaHurricane <maryishch88@gmail.com>
2026-02-19 23:24:20 +0000

    delete extra page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,30 +1,4 @@
-// import { fetchNotes } from "@/lib/api";
-// import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
-// import NotesClient from "../../Notes.client";
 
-// interface FilterPageProps {
-//     params: Promise<{ slug: string[] }>
-// }
-
-// export default async function FilterPage( { params }: FilterPageProps) {
-//     const { slug } = await params;
-//     const category = slug[0];
-
-//     const queryClient = new QueryClient();
-
-//     await queryClient.prefetchQuery({
-//         queryKey: ["notes", { category }],
-//         queryFn: () => fetchNotes(1, "", category),
-//     });
-
-//     return (
-//         <>
-//         <HydrationBoundary state={dehydrate(queryClient)}>
-//             <NotesClient />
-//         </HydrationBoundary>
-//         </>
-//     )
-// }
 
 import { fetchNotes } from  "@/lib/api";
 import {
@@ -32,7 +6,7 @@ import {
   dehydrate,
   HydrationBoundary,
 } from "@tanstack/react-query";
-import NotesClient from "@/app/notes/filter/[...slug]/Notes.client";
+import NotesClient from "./Notes.client";
 
 
 export default async function Notelist() {
