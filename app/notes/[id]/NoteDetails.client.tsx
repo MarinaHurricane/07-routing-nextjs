0c21aa58db6ce0880337acb3126130e72d02a675
MarinaHurricane <maryishch88@gmail.com>
2026-02-21 14:39:20 +0000

    Modal props add

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -3,12 +3,15 @@
 import { fetchNoteById } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import css from "./NoteDetails.module.css";
+import Modal from "@/components/Modal/Modal";
+import { useRouter } from "next/navigation";
 
 interface NoteDetailsClientProps {
   id: string;
 }
 
 export default function NoteDetailsClient( { id }: NoteDetailsClientProps) {
+  const router = useRouter();
 
   const {
     data: note,
@@ -25,6 +28,7 @@ export default function NoteDetailsClient( { id }: NoteDetailsClientProps) {
   if (!note) return <p>Something went wrong.</p>;
 
   return (
+    <Modal onClose={() => router.back()}>
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
@@ -35,6 +39,7 @@ export default function NoteDetailsClient( { id }: NoteDetailsClientProps) {
         <p className={css.date}>{note.createdAt}</p>
       </div>
     </div>
+    </Modal>
   );
 }
 
